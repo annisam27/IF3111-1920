29a6a32bd4a002facd9f7e3699dd54fb9cd53206
Nurul fauzia aziza <fauziaazizahnurul@gmail.com>
2019-12-13 21:17:15 +0700

    Update m_data.php

--- SIMPLELAPOR/application/models/m_data.php
+++ SIMPLELAPOR/application/models/m_data.php
@@ -1,21 +1,74 @@
-<?php 
+<?php
+<<<<<<< HEAD
+class M_data extends CI_Model
+{
+    public function tampil_data()
+    {
+        $data = $this->db->query('select * from laporan')->result_array();
+        return $data;
+    }
 
-class M_data extends CI_Model{
-	function tampil_data(){
+    public function input_data($data, $table)
+    {
+        $this->db->insert($table, $data);
+    }
+
+    public function tampil()
+    {
+        $query = $this->db->get('laporan');
+        if ($query->num_rows() > 0) {
+            return $query->result();
+        } else {
+            return array();
+        }
+    }
+
+    function per_id($id_laporan)
+    {
+        $this->db->where('id_laporan', $id_laporan);
+        $query = $this->db->get('laporan');
+        return $query->result();
+    }
+=======
+
+class M_data extends CI_Model
+{
+	public function tampil_data()
+	{
 		return $this->db->get('laporan');
 	}
 
-	function input_data($data,$table){
-		$this->db->insert($table,$data);
-    }
-    
-	function hapus_data($where,$table){
+	public function input_data($data, $table)
+	{
+		$this->db->insert($table, $data);
+	}
+	public function tampil()
+	{
+		$query = $this->db->get('laporan');
+		if ($query->num_rows() > 0) {
+			return $query->result();
+		} else {
+			return array();
+		}
+	}
+
+	public function per_id($id_laporan)
+	{
+		$this->db->where('id_laporan', $id_laporan);
+		$query = $this->db->get('laporan');
+		return $query->result();
+	}
+
+	public function hapus_data($where, $table)
+	{
 		$this->db->where($where);
 		$this->db->delete($table);
-    }
-    
-    function update_data($where,$data,$table){
+	}
+
+	public function update_data($where, $data, $table)
+	{
 		$this->db->where($where);
-		$this->db->update($table,$data);
-	}	
+		$this->db->update($table, $data);
+	}
+>>>>>>> 31fa7b1bd383f1b093f50e3a825187d22d835ecb
 }
