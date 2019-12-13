8f9d183378c6df6a62d9985af52d19a8592e09a7
Annisa Maharani <maharaniannisa80@gmail.com>
2019-12-13 20:55:42 +0700

    ADD view/v_tampil.php

---
+++ SIMPLELAPOR/application/views/v_tampil.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+</head>
+
+<body>
+	<center>
+		<h1>Daftar Laporan</h1>
+	</center>
+	<center><?php echo anchor('crud/tambah_aksi', 'Buat Laporan'); ?></center>
+	<table style="margin:20px auto;" border="1">
+
+
+		<?php $no = 1;
+		foreach ($laporan as $u) : ?>
+			<h1><?php echo $u['Aspek'] ?></h1>
+			<p><?php echo $u['Laporan_Komentar'] ?></p>
+			<small>Lampiran :<?php echo $u['Lampiran'] ?></small><br>
+			<small>Aspek: <?php echo $u['Aspek'] ?></small>
+			&nbsp;
+			<small>Waktu: <?php echo $u['Waktu'] ?></small>
+			<hr />
+			<br>
+		<?php endforeach; ?>
+	</table>
+</body>
+
+</html>
