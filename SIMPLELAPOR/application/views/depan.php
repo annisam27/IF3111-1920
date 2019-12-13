33f0224d30bd88def9ea4a7b508ac5ce21391e51
Annisa Maharani <maharaniannisa80@gmail.com>
2019-12-14 02:23:19 +0700

    update depan.php

--- SIMPLELAPOR/application/views/depan.php
+++ SIMPLELAPOR/application/views/depan.php
@@ -1,44 +1,37 @@
 <section>
-<nav>
-				<ul>
-					<li> <a href="<?php echo base_url().'index.php/web' ?>">Daftar</a></li>
-					<li><a href="<?php echo base_url().'index.php/web/login' ?>">Masuk</a></li>
-				</ul>
-			</nav>
-<!-- MODAL ADD -->
-<div class="modal fade" id="ModalaAdd" tabindex="-1" role="dialog" aria-labelledby="largeModal" aria-hidden="true">
-                <p class="modal-title" id="myModalLabel">Buat Laporan/Komentar</p>
-                <hr />
-            </div>
-            <form action="<?php echo base_url(). 'crud/tambah_aksi'; ?>" method="post">
-            <form class="form-horizontal">
-                <div class="modal-body">
-                    <div class="form-group">
-                        <textarea name="Laporan_Komentar" id="Laporan_Komentar" class="form-control" type="text" placeholder="Laporan/Komentar" rows="15" cols="120" required></textarea>
-                    </div>
-                    
-                    <div class="form-group">
+    <!-- MODAL ADD -->
+    <div class="modal fade" id="ModalaAdd" tabindex="-1" role="dialog" aria-labelledby="largeModal" aria-hidden="true">
+        <p class="modal-title" id="myModalLabel">Buat Laporan/Komentar</p>
+        <hr />
+    </div>
+    <form action="<?php echo base_url() . 'crud/tambah_aksi'; ?>" method="post">
+        <form class="form-horizontal">
+            <div class="modal-body">
+                <div class="form-group">
+                    <textarea name="Laporan_Komentar" id="Laporan_Komentar" class="form-control" type="text" placeholder="Laporan/Komentar" rows="15" cols="120" required></textarea>
+                </div>
+
+                <div class="form-group">
                     <select name="Aspek">
-                        <option style="display:none" value= 'pilih'> Pilih Aspek Pelaporan/Komentar</option>
-                        <option value='Infrastruktur'>Infrastrktur</option>
-                        <option value='Kesehatan'>Kesehatan</option>
-                        <option value='Kriminal'>Kriminal</option>
-                        <option value='Pendidikan'>Pendidikan</option>
-                        <option value='Administrasi'>Administrasi</option>
-                        <option value='Bantuan'>Bantuan</option>
+                        <option style="display:none" value='pilih'> Pilih Aspek Pelaporan/Komentar</option>
+                        <option value='Dosen'>Dosen</option>
+                        <option value='Staff'>Staff</option>
+                        <option value='Mahasiswa'>Mahasiswa</option>
+                        <option value='Infrastruktur'>Infrastruktur</option>
+                        <option value='Pengajaran'>Pengajaran</option>
                     </select>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-xs-9">
+                        <br><br> <input name="Lampiran" id="Lampiran" class="form-control" type="file" placeholder="Choose File" style="width:335px;" required>
                     </div>
-                
-                    <div class="form-group">
-                        <div class="col-xs-9">
-                          <br><br>  <input name="Lampiran" id="Lampiran" class="form-control" type="file" placeholder="Choose File" style="width:335px;" required>
-                        </div>
-                    </div>
+                </div>
 
                 <div class="modal-footer">
-                   <br><br> <button  align= "right" class="btn btn-info" id="btn_simpan">Buat Lapor!</button>
+                    <br><br> <button align="right" class="btn btn-info" id="btn_simpan">Buat Lapor!</button>
                 </div>
-            </form>
-            </form>
-            </div>
-        <!--END MODAL ADD-->
+        </form>
+    </form>
+    </div>
+    <!--END MODAL ADD-->
