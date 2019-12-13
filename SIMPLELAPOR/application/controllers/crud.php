9cc79120cf12e84ea3b355b8a569459b5cfc6924
Nurtias Rahayu <nurtias.14117086@student.itera.ac.id>
2019-12-13 16:17:07 +0700

    ADD crud.php

---
+++ SIMPLELAPOR/application/controllers/crud.php
@@ -0,0 +1,36 @@
+<?php 
+
+
+class Crud extends CI_Controller{
+
+	function __construct(){
+		parent::__construct();		
+		$this->load->model('m_data');
+		$this->load->helper('url');
+
+	}
+
+	function index(){
+		$data['laporan'] = $this->m_data->tampil_data()->result();
+		$this->load->view('v_tampil',$data);
+	}
+
+	function tambah(){
+		$this->load->view('depan');
+	}
+
+	function tambah_aksi(){
+		$Laporan_Komentar = $this->input->post('Laporan_Komentar');
+		$Aspek = $this->input->post('Aspek');
+		$Lampiran = $this->input->post('Lampiran');
+
+		$data = array(
+			'Laporan_Komentar' => $Laporan_Komentar,
+			'Aspek' => $Aspek,
+			'Lampiran' => $Lampiran
+			);
+		$this->m_data->input_data($data,'laporan');
+		redirect('crud/index');
+	}
+
+}
