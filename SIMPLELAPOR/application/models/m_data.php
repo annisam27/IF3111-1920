f481a1da551b5e61ebd8116e7757d0d94a7e65fa
Nurtias Rahayu <nurtias.14117086@student.itera.ac.id>
2019-12-13 16:27:46 +0700

    UPDATE m_data.php

--- SIMPLELAPOR/application/models/m_data.php
+++ SIMPLELAPOR/application/models/m_data.php
@@ -7,4 +7,8 @@ class M_data extends CI_Model
         $data = $this->db->query('select * from laporan')->result_array();
         return $data;
     }
+
+    public function input_data($data,$table){
+		$this->db->insert($table,$data);
+	}
 }
