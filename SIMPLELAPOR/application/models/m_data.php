958a00f6505595db9ad03b2ce87dd9e12c7a9fd5
Annisa Maharani <maharaniannisa80@gmail.com>
2019-12-13 20:47:32 +0700

    CHANGE m_data.php

--- SIMPLELAPOR/application/models/m_data.php
+++ SIMPLELAPOR/application/models/m_data.php
@@ -1,21 +1,42 @@
-<?php 
+<?php
 
-class M_data extends CI_Model{
-	function tampil_data(){
+class M_data extends CI_Model
+{
+	public function tampil_data()
+	{
 		return $this->db->get('laporan');
 	}
 
-	function input_data($data,$table){
-		$this->db->insert($table,$data);
-    }
-    
-	function hapus_data($where,$table){
+	public function input_data($data, $table)
+	{
+		$this->db->insert($table, $data);
+	}
+	public function tampil()
+	{
+		$query = $this->db->get('laporan');
+		if ($query->num_rows() > 0) {
+			return $query->result();
+		} else {
+			return array();
+		}
+	}
+
+	public function per_id($id_laporan)
+	{
+		$this->db->where('id_laporan', $id_laporan);
+		$query = $this->db->get('laporan');
+		return $query->result();
+	}
+
+	public function hapus_data($where, $table)
+	{
 		$this->db->where($where);
 		$this->db->delete($table);
-    }
-    
-    function update_data($where,$data,$table){
+	}
+
+	public function update_data($where, $data, $table)
+	{
 		$this->db->where($where);
-		$this->db->update($table,$data);
-	}	
+		$this->db->update($table, $data);
+	}
 }
