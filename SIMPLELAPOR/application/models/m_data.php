6d13ce24c9f0784ecf2ab7eda41d124092d126c6
Nurul fauzia aziza <fauziaazizahnurul@gmail.com>
2019-12-13 17:13:51 +0700

    Update m_data.php

--- SIMPLELAPOR/application/models/m_data.php
+++ SIMPLELAPOR/application/models/m_data.php
@@ -1,14 +1,16 @@
-<?php
+<?php 
 
-class M_data extends CI_Model
-{
-    public function tampil_data()
-    {
-        $data = $this->db->query('select * from laporan')->result_array();
-        return $data;
-    }
+class M_data extends CI_Model{
+	function tampil_data(){
+		return $this->db->get('laporan');
+	}
 
-    public function input_data($data,$table){
+	function input_data($data,$table){
 		$this->db->insert($table,$data);
+    }
+    
+	function hapus_data($where,$table){
+		$this->db->where($where);
+		$this->db->delete($table);
 	}
 }
